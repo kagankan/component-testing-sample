e8a1e0e256617c846e51e5ef5fb342a9fece92e1
Kishi Yoshimaru <yoshimaru.kishi@kikagaku.co.jp>
2025-06-12 03:18:24 +0900

    vitest browser mode をセットアップ

---
+++ src/components/Sample.vitest-browser.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
+import { Sample } from './Sample';
+
+test('loads and displays Sample', async () => {
+  // Render a React element into the DOM
+  const screen = render(<Sample />);
+
+  await expect.element(screen.getByText('Sample')).toBeVisible();
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   test: {
-    include: ['**/*.vitest.test.tsx'],
-    globals: true, // global: true にしないと、 testing-library/jest-dom 内部の参照が失敗する
+    
+    projects: [
+      {
+        test: {
+          name: 'jsdom',
+          include: ['**/*.vitest.test.tsx'],
+          globals: true, // global: true にしないと、 testing-library/jest-dom 内部の参照が失敗する
+        },
+      },
+      {
+        plugins: [react()],
+        test: {
+          name: 'browser',
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            instances: [{ browser: 'chromium' }],
+          },
+          include: ['**/*.vitest-browser.test.tsx'],
+        },
+      },
+    ],
   },
 })
