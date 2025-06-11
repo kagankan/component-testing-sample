290d74e368ed6116c3bc6b8bcab2d8c7d5a75c32
Kishi Yoshimaru <yoshimaru.kishi@kikagaku.co.jp>
2025-06-12 03:28:21 +0900

    storybookでのテストをセットアップ

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,53 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
-    
     projects: [
       {
         test: {
-          name: 'jsdom',
-          include: ['**/*.vitest.test.tsx'],
+          name: "jsdom",
+          include: ["**/*.vitest.test.tsx"],
           globals: true, // global: true にしないと、 testing-library/jest-dom 内部の参照が失敗する
         },
       },
       {
         plugins: [react()],
         test: {
-          name: 'browser',
+          name: "browser",
           browser: {
             enabled: true,
-            provider: 'playwright',
-            instances: [{ browser: 'chromium' }],
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
           },
-          include: ['**/*.vitest-browser.test.tsx'],
+          include: ["**/*.vitest-browser.test.tsx"],
+        },
+      },
+      {
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
         },
       },
     ],
   },
-})
+});
