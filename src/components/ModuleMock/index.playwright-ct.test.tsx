f8421d07ec97ff230ae0e071f7c935872a4c0b19
Kishi Yoshimaru <yoshimaru.kishi@kikagaku.co.jp>
2025-06-15 17:20:16 +0900

    Playwright でモジュールモックはできない

--- src/components/ModuleMock/index.playwright-ct.test.tsx
+++ src/components/ModuleMock/index.playwright-ct.test.tsx
@@ -1,16 +1,13 @@
 import { expect, test } from "@playwright/experimental-ct-react";
 import { ModuleMockSample } from ".";
-import { vi } from "vitest";
-
-vi.mock("./sample-module", () => ({
-  someFunction: vi.fn().mockReturnValue("mocked"),
-}));
 
 test.describe("モジュールモック", () => {
-  test("Playwright ではモジュールモックは不可能なはず", async ({ mount }) => {
+  // ❌️ Playwright ではモジュールモックは不可能
+  test("モジュールモックは不可能", async ({ mount }) => {
     const component = await mount(<ModuleMockSample />);
 
     // 表示されているテキストを確認
-    await expect(component.getByText("mocked")).toBeVisible();
+    // await expect(component.getByText("mocked")).toBeVisible();
+    await expect(component.getByText("original")).toBeVisible();
   });
 });
